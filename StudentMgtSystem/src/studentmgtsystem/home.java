eb4b1af7cb7cadb4e77c0d6ba380db183d8d53cb
TMWEE <pathmikaweerarathna@gmail.com>
2020-09-27 00:19:56 +0530

    Student CRUD done

--- StudentMgtSystem/src/studentmgtsystem/home.java
+++ StudentMgtSystem/src/studentmgtsystem/home.java
@@ -274,6 +274,9 @@ public class home extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        setVisible(false);
+        studentCRUD object = new studentCRUD();
+        object.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
